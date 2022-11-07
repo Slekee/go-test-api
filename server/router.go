5cd1f5fd2dfd64f2d9176a7634592eef080b892c
Erkko Sallinen <erkko.sallinen@gmail.com>
2022-11-08 01:32:19 +0200

    server config

---
+++ server/router.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"test-api/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter() *gin.Engine {
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	health := new(controllers.HealthController)
+	router.GET("/health", health.Status)
+
+	v1 := router.Group("v1")
+	{
+		v1.GET("/ping", controllers.Ping)
+	}
+
+	return router
+}
